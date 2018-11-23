6ce31aa2ee0a00a00d20dbddbe0c5d08c8b388a8
teru <18109251+Terunori@users.noreply.github.com>
2018-11-23 17:45:09 +0900

    update readme for BME280

--- test_BME280/test_BME280.ino
+++ test_BME280/test_BME280.ino
@@ -8,12 +8,53 @@
 
 Adafruit_BME280 bme; // I2C
 
+unsigned long delayTime;
+
 void setup()
 {
-    // put your setup code here, to run once:
+    Serial.begin(9600);
+    Serial.println(F("BME280 test"));
+
+    bool status;
+
+    // default settings
+    // (you can also pass in a Wire library object like &Wire2)
+    status = bme.begin();
+    if (!status) {
+        Serial.println("Could not find a valid BME280 sensor, check wiring!");
+        while (1);
+    }
+
+    Serial.println("-- Default Test --");
+    delayTime = 1000;
+
+    Serial.println();
 }
 
 void loop()
 {
-    // put your main code here, to run repeatedly:
+    printValues();
+    delay(delayTime);
+}
+
+void printValues()
+{
+    Serial.print("Temperature = ");
+    Serial.print(bme.readTemperature());
+    Serial.println(" *C");
+
+    Serial.print("Pressure = ");
+
+    Serial.print(bme.readPressure() / 100.0F);
+    Serial.println(" hPa");
+
+    Serial.print("Approx. Altitude = ");
+    Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
+    Serial.println(" m");
+
+    Serial.print("Humidity = ");
+    Serial.print(bme.readHumidity());
+    Serial.println(" %");
+
+    Serial.println();
 }
