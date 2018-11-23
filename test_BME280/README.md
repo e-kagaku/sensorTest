daa4ecaa0517fea892dcca568c9fa156b0a84f00
teru <18109251+Terunori@users.noreply.github.com>
2018-11-23 17:34:46 +0900

    update README.md

--- test_BME280/README.md
+++ test_BME280/README.md
@@ -9,5 +9,12 @@ written on 2018/11/23, by Ihara
 
 ## Library
 
-https://github.com/adafruit/Adafruit_BME280_Library
+- https://github.com/adafruit/Adafruit_BME280_Library
 
+## Usage
+
+- I2Cで、5番ピンをVDDに接続して配線してください
+- I2Cのアドレス選択は[0x77]となります
+- 以下に配線図を示します
+
+![BME280配線](img/BME280_I2C_connect.jpg)
---
+++ test_BME280/test_BME280.ino
@@ -0,0 +1,19 @@
+
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
+
+#define SEALEVELPRESSURE_HPA (1013.25)
+
+Adafruit_BME280 bme; // I2C
+
+void setup()
+{
+    // put your setup code here, to run once:
+}
+
+void loop()
+{
+    // put your main code here, to run repeatedly:
+}
